160389ca1678b5399a2d5eb2193679c18975ea50
mmusaev <9410421+mmusaev@users.noreply.github.com>
2024-10-14 08:26:04 -0500

    feat: intial commit

--- FuncAppApim/Function1.cs
+++ FuncAppApim/Function1.cs
@@ -14,7 +14,7 @@ namespace FuncAppApim
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "v1/Function1")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
